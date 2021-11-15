668bef345e0be5b0f64707f7bd95b7cbf3ddfe5b
Brian Parker <btparker70@gmail.com>
2021-11-15 00:30:43 -0800

    added components and containers

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// document.getElementById('root')
+// in index.html, this is the main div with an id of 'root'
+// the entire app is rendered here...
