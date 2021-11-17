a4c2106ec122d12e6228b83a8cc53dc0a270a653
Brian Parker <btparker70@gmail.com>
2021-11-16 19:12:17 -0800

    added css to app.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Footer, Blog, Possibility, Features, WhatGTP3, Header } from './containers'
 import { Cta, Brand, Navbar } from './components'
+import './App.css'
 
 const App = () => {
   return (
