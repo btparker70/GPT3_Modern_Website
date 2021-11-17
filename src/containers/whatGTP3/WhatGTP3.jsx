d496510b37b6700712dbe7bfd1e18f6ba91f9272
Brian Parker <btparker70@gmail.com>
2021-11-16 22:54:13 -0800

    brand, some whatgpt3

--- src/containers/whatGTP3/WhatGTP3.jsx
+++ src/containers/whatGTP3/WhatGTP3.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Feature } from '../../components/feature'
 import './whatGTP3.css'
 
 const WhatGTP3 = () => {
   return (
-    <div>
-      WhatGTP3
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+     <div className="gpt3__whatgpt3-feature">
+        <Feature />
+     </div>
+     <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+        The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+     </div>
     </div>
   )
 }
